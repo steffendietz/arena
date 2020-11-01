3ed35bfeb7829cf74d85815739a4d6476bf0e159
Steffen Dietz <steffo.dietz@googlemail.com>
2020-11-01 19:41:58 +0000

    added ticker service added matching dispatcher

--- app/src/Directive/ActorDirective.php
+++ app/src/Directive/ActorDirective.php
@@ -1,8 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Directive;
 
-use App\Database\User;
 use Spiral\Prototype\Traits\PrototypeTrait;
 use Spiral\Stempler\Directive\AbstractDirective;
 use Spiral\Stempler\Node\Dynamic\Directive;
