27d02aaa91b5f72372a3171d90e7c3aef6c8e7ca
steffendietz <steffo.dietz@googlemail.com>
2022-03-29 23:48:59 +0000

    using only released packages added phpstan

---
+++ app/src/Bootloader/BroadcastBootloader.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Bootloader;
+
+use Spiral\Boot\Bootloader\Bootloader;
+use Spiral\Core\Container;
+use Spiral\Goridge\RPC\RPCInterface;
+use Spiral\RoadRunner\Broadcast\Broadcast;
+use Spiral\RoadRunner\Broadcast\BroadcastInterface;
+use Spiral\RoadRunnerBridge\Bootloader\RoadRunnerBootloader;
+
+final class BroadcastBootloader extends Bootloader
+{
+    protected const DEPENDENCIES = [
+        RoadRunnerBootloader::class
+    ];
+
+    public function boot(Container $container): void
+    {
+        $this->registerBroadcast($container);
+    }
+
+    private function registerBroadcast(Container $container)
+    {
+        $container->bind(BroadcastInterface::class, Broadcast::class);
+        $container->bindSingleton(
+            Broadcast::class,
+            function (RPCInterface $rpc): Broadcast {
+                return new Broadcast($rpc);
+            }
+        );
+    }
+}
---
+++ app/src/polyfill.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+class_alias(
+    \App\Bootloader\BroadcastBootloader::class,
+    \Spiral\Bootloader\Broadcast\BroadcastBootloader::class
+);
